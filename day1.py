0ff772c4131a95929a23f4ec53eaa9b81ea19605
May Sophia <may.philadelphia@gmail.com>
2021-12-01 20:42:52 -0500

    Code reuse!

--- day1.py
+++ day1.py
@@ -13,22 +13,19 @@ def iter_window(it: Iterable[T], n: int) -> Iterable[Tuple[T, ...]]:
     return zip(*tees)
 
 
-def puzzle1(data: Iterable[int]) -> int:
-    return sum(1 for _ in (filter(lambda i: i[0] < i[1], pairwise(data))))
-
-
-def puzzle2(data: Iterable[int], win_len: int = 3) -> int:
+def count_decreasing_windows(data: Iterable[int], win_len: int) -> int:
     return sum(1 for _ in (filter(lambda i: i[0] < i[1], pairwise(sum(w) for w in iter_window(data, win_len)))))
 
 
 if __name__ == "__main__":
     args = sys.argv
+    print(args)
     if len(args) < 2:
         print("Please provide input file")
         exit(1)
     with open(sys.argv[1]) as f:
         data = [int(l) for l in f.readlines()]
         start = time.time()
-        print(f"1st puzzle solution: {puzzle1(data)} in {time.time()-start:.6f} seconds")
+        print(f"1st puzzle solution: {count_decreasing_windows(data, 1)} in {time.time()-start:.6f} seconds")
         start = time.time()
-        print(f"2nd puzzle solution: {puzzle2(data)} in {time.time()-start:.6f} seconds")
+        print(f"2nd puzzle solution: {count_decreasing_windows(data, 3)} in {time.time()-start:.6f} seconds")
